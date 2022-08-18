c95b3d504b1a7cf1e2d9e6540c90b10a7b34f6d5
Parmeet <parmeet.97637@gmail.com>
2022-08-18 14:15:34 +0530

    K20MP updated

--- K20-MP/K20-MP.sql
+++ K20-MP/K20-MP.sql
@@ -121,4 +121,155 @@ select * from employee where gender='M' order by age desc;
 -- select all the employees and sort them on basis of salary in desc order
 select * from employee order by salary desc;
 
+-- Aggregate Functions : Numerical columns, 
+-- Will return single values
+-- max, min, count, avg
 
+-- select the maximum salary of the employees
+select max(salary) from employee;
+
+-- select minimum age of the employee
+select min(age) from employee;
+
+-- select avg salary of employee
+select avg(salary) from employee;
+
+-- count the number of employees working in an organization
+select count(ID) as no_of_employee from employee;
+
+-- find the unique age 
+select distinct(age) from employee;
+
+-- count the employees with unique age
+select count(distinct(age)) from employee;
+
+-- Group By Clause
+-- Pandas : pd.pivot_table, pd.groupby
+-- excel : pivots
+-- sql : group by
+-- order : select, from, where, group by
+
+-- select maximum salary for male and female employee
+select max(salary),Gender from employee group by Gender;
+-- excluding NULL
+select max(salary),Gender from employee where Gender is not null group by Gender;
+-- include only NULL
+select max(salary),Gender from employee where Gender is null group by Gender;
+
+-- select average salary of employees in each age group
+select age,avg(salary) as average_salary from employee group by age;
+
+-- select average salary of employees in each age group only for males
+select age, avg(salary) as avg_sal from employee where Gender='M' group by age;
+
+-- select average salary of employees in each age group only for males who are having avg salary greater than 85K
+-- Having clause : 
+--              1. It is used with group by
+--              2. It is used to apply filters on the aggregate functions
+
+-- order: select, from, where, group by, having
+select age, avg(salary) as avg_sal from employee where Gender='M' group by age having avg(salary)>85000;
+
+-- Views 
+-- 1. They are read only. You cannot update views
+-- 2. View is a virtual table created as  a result of operations on a single or multiple tables
+-- 3. Advantage of view: Data security as it is read only mode
+
+-- create view for all the male employees
+create view employee_view 
+as 
+select * from employee where Gender='M';
+
+-- check : All male records are created in employee_view
+select * from employee_view;
+
+-- delete the view
+drop view employee_view;
+
+-- Nested Queries or Sub queries
+-- Applying referential contraint to the below table
+create table personal_details (
+SNo integer,
+mob_no int8,
+bank_balance decimal(12,2),
+city varchar(20),
+address_type varchar(20),
+constraint Sno foreign key(Sno) references employee(ID)
+);
+
+
+-- insert the records
+insert into personal_details 
+VALUES(1,9999999999,6000,'Indore','Permanent'),(1,9989898988,700000,'Pune','Current'),
+(2,9999888876,650,'Bhopal','Permanent'),
+(5,1234567890,1200,'Pune','Permanent'),
+(4,9876543234,45000,'Bengaluru','Current');
+
+-- select the employees where salary is greater than avg bank_balance
+select * from employee where salary>
+(select avg(bank_balance) from personal_details);
+
+-- joins
+-- Inner Join
+-- Left join
+-- Right Join
+-- Outer join or Full outer join
+-- Cross Join
+
+-- inner join on employee and personal_details
+select * from employee e inner join personal_details p on e.Id=p.Sno; 
+
+select e.ID,e.Name,e.Gender,e.salary,sum(p.bank_balance)  as total_balance from employee e inner join personal_details p on e.Id=p.Sno group by Sno; 
+
+-- right join
+select * from employee e right join personal_details p on e.Id = p.Sno;
+
+-- left join
+select * from employee e left join personal_details p on e.Id = p.Sno;
+
+-- union
+create table details (
+ID integer,
+mob_no int8,
+bank_balance decimal(12,2),
+city varchar(20),
+address_type varchar(20),
+constraint ID foreign key(ID) references employee(ID)
+);
+
+insert into details 
+VALUES(3,9999999999,6000,'Patna','Permanent'),(3,9989898988,700000,'Pune','Current');
+
+insert into details VALUES(2,9999888876,650,'Bhopal','Permanent');
+
+-- union
+-- exclude the duplicate values
+select * from personal_details
+union
+select * from details;
+ 
+ -- union all
+ -- it will include the duplicate values
+ select * from personal_details
+union all
+select * from details;
+
+ 
+ -- view for above tables
+ create view personal_details_view
+ as
+ 
+select * from personal_details
+union
+select * from details;
+
+-- check
+select * from personal_details_view;
+ 
+-- outer  join
+select * from employee e right join personal_details p on e.Id = p.Sno
+union
+select * from employee e left join personal_details p on e.Id = p.Sno;
+
+-- cross join
+select count(*) from employee e cross join personal_details p;
