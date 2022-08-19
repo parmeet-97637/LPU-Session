376e3dbd678d61ef13bd4d61875bf0cb277f886b
Parmeet <parmeet.97637@gmail.com>
2022-08-19 11:08:44 +0530

    K20CH updated

--- K20-CH/K20-CH.sql
+++ K20-CH/K20-CH.sql
@@ -156,7 +156,7 @@ select count(distinct age) from employee;
 -- pandas : pd.groupby, pd.pivot_table
 -- excel : pivot_table
 -- order : select, from, where, group by,order by
--- withe group by you would have aggregate function
+-- with group by you would have aggregate function
 
 -- select average age of male and female
 select gender, avg(age) as avg_age from employee group by gender order by avg_age;
@@ -167,7 +167,7 @@ select age, avg(salary) from employee group by age;
 -- find the count of male and female employees
 select gender,count(name) as count_of_employee from employee group by gender;
 
--- select the average salary of male employees for each group
+-- select the average salary of male employees for each ahe group
 select avg(salary), age from employee where gender='M' group by age;
 
 -- Having clause
@@ -220,10 +220,202 @@ select dayofmonth(current_date());
 
 -- convert string date to date type
 select str_to_date('10,03,2022','%d,%m,%Y');
-select str_to_date('10,03,22','%d,%m,%y');
 
 -- select all the employee where we donot have any null values
 select * from employee where age is not null;
 
 -- select all employees where age is missing
 select * from employee where age is null;
+
+-- Views
+-- 1. Views are read only
+-- 2. Views are the virtual tables created from a single or multiple tables
+-- 3. Advanctages : Data Security as it is read only
+
+create view employee_view
+as
+select * from employee where gender='M';
+
+-- check
+select * from employee_view;
+
+-- drop the view
+drop view employee_view;
+
+-- Nested queries or sub queries
+create table personal_details (
+Sno integer,
+bank_balance decimal(12,2),
+city varchar(20),
+address_type varchar(20),
+constraint Sno foreign key(Sno) references employee(ID)
+);
+
+-- insert the records
+insert into personal_details VALUES(1,4567,'Indore','Permanent'),(1,1200,'Pune','Current'),(2,6789,'Pune','Permaenent'),(3,8900,'Bengaluru','Current');
+insert into personal_details VALUES(4,45607,'Bhopal','Permanent'),(1,12000,'Chennai','Current'),(5,6789,'Mumbai','Current');
+insert into personal_details VALUES(6,4560907,'Bhopal','Permanent');
+-- check
+select * from personal_detail;
+select avg(bank_balance) from personal_details;
+-- select the employees whose salary is greater than avg(bank_balance) of all employees
+select * from employee where salary > (select avg(bank_balance) from personal_details);
+
+-- Joins
+-- Inner join
+-- Outer Join
+-- Right Join
+-- Left Join
+
+-- Inner join
+-- select the employees whose personal details are present
+select * from employee e inner join personal_details p  on e.ID = p.Sno;
+
+-- select the employees with their total bank balance
+select e.Name, e.Salary, e.Age, e.Joining_date, sum(p.bank_balance) as total_balance
+from employee e inner join personal_details p on e.Id=p.Sno
+group by p.Sno;
+
+-- right join
+select * from employee e right join personal_details p on e.Id=p.Sno;
+
+-- left join
+select * from employee e left join personal_details p on e.Id=p.Sno;
+
+-- union
+create table details (
+s_no integer,
+bank_balance decimal(12,2),
+city varchar(20),
+address_type varchar(20),
+constraint s_no foreign key(s_no) references employee(ID)
+);
+
+-- insert the record
+insert into details values(7,123459,'Pune','Current'), (6,4560907,'Bhopal','Permanent');
+
+-- check
+select * from details;
+
+-- union
+-- 1. Structure of the tables to combine should be same
+-- 2. It will remove/exclude duplicate records
+select * from personal_details
+union
+select * from details;
+
+-- union ALL
+-- 1. It will include the duplicate records
+
+select * from personal_details
+union ALL
+select * from details;
+
+-- create a view for above table
+create view personal_details_view
+as
+select * from personal_details
+union ALL
+select * from details;
+
+-- check
+select * from personal_details_View;
+
+-- select the duplicate Ids
+select sno,count(sno)from personal_details_view group by sno having count(sno)>1;
+
+-- duplicate records
+select sno, count(sno) from personal_details_view 
+group by sno, bank_balance, city, address_type having count(sno)>1;
+
+-- outer join
+-- 1. It is the combination/union of left and right join
+
+select * from employee e right join personal_details p on e.Id = p.Sno
+union
+select * from employee e left join personal_details p on e.Id = p.Sno;
+
+-- cross join
+select count(*) from employee e cross join personal_details p;
+
+-- update function
+ -- updating the Sno from 1 to 2 where city is chennai
+ 
+ select * from personal_details where Sno=1 and city='chennai';
+
+-- syntax : update table_name set column_name=value where condition
+
+-- trun on safe sql updates
+SET SQL_SAFE_UPDATES =0;
+
+update personal_details set Sno =2 where Sno=1 and city='chennai';
+
+-- check 
+select * from personal_details where Sno=1 and city='chennai';
+select * from personal_details where Sno=2 and city='chennai';
+-- trun off safe sql updates
+SET SQL_SAFE_UPDATES =1;
+
+-- update the age of missing employees to 21
+select * from employee where age is NULL;
+
+# turn on sql safe updates
+SET SQL_SAFE_UPDATES =0;
+
+# update
+update employee set Age=21  where age is NULL;
+
+# check table
+select * from employee where age is NULL;
+select * from employee where age =21;
+
+-- Alter function
+-- 1. It is used to alter the table
+-- changing the data type of age from smallint to int
+alter table employee
+modify column Age integer;
+
+-- add a new column in personal details : emailid
+alter table personal_details
+ADD column emailid varchar(25);
+
+-- check
+select * from personal_details;
+
+-- update
+update personal_details set emailid ='p@gmail.com' where Sno=1;
+update personal_details set emailid ='s@gmail.com' where Sno=2;
+update personal_details set emailid ='k@gmail.com' where Sno=3;
+update personal_details set emailid ='l@gmail.com' where Sno=4;
+update personal_details set emailid ='m@gmail.com' where Sno=5;
+
+-- limit
+-- sleect first 5 records from employee
+select * from employee limit 5;
+
+-- select the employee with second highest salary
+select * from employee order by salary desc limit 2;
+
+# add offset to limit
+select * from employee order by salary desc limit 1,1;
+
+-- sleect the third highest salary
+select * from employee order by salary desc limit 2,1;
+
+-- exists
+# syntax : select * from table_name where exists (select coumn_name from table_name where condition)
+
+-- select all the employees personal details where ID =1
+select * from employee where exists (select bank_balance from personal_details where id=1);
+
+-- select all the employees personal details where sum of bank_balance is greaer than 50K
+select * from employee where exists(select sum(bank_balance) from personal_details where city ='Chennai' group by sno having sum(bank_balance)>5000);
+
+-- Any operator
+-- getting employees Id where bank_balance is greater than 50K
+select * from employee where ID = ANY (select Sno from personal_details where bank_balance>50000);
+select * from employee where ID = ANY (select Sno from personal_details where address_type='Permanent');
+
+-- ALL
+select * from employee where ID = ALL (select Sno from personal_details where address_type='Permanent');
+
